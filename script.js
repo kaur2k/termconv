7593a8ae818695cdf9bf637faf94a3e105d54f70
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-04-07 13:57:16 +0300

    elasticsearch import

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 var fs     = require('fs')
 var xml2js = require('xml2js')
 
+var elasticsearch = require('elasticsearch')
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+})
+
+
 var parser = new xml2js.Parser()
 
-fs.readFile('militerm.xml', function(err, data) {
+fs.readFile('mili_mt.xml', 'utf16le', function(err, data) {
+    if (err) {
+        return console.log(err)
+    }
+    // console.log(data)
     parser.parseString(data, function (err, result) {
-        console.dir(result)
+        if (err) {
+            return console.log(err)
+        }
+        var conceptGroupA = result.mtf.conceptGrp
+
+        conceptGroupA = conceptGroupA.map(function(c) {
+            return {
+                index: {_index: 'concept', _type: 'concept', _id: c.concept[0] },
+                conceptGroup: c
+            }
+        })
+
+        client.bulk({ body: conceptGroupA })
+
+        // var json212 = conceptGroupA[100]
+        // console.log(JSON.stringify(json212, null, 4))
+        //
+        // var builder = new xml2js.Builder()
+        // var xml212 = builder.buildObject(json212)
+        // console.log(xml212)
+
+        // console.dir(result)
         console.log('Done')
     })
 })
