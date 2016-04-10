8a58ef97f9669984427f199531208797632e7785
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-04-10 13:10:12 +0300

    import via mappings

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
-var fs     = require('fs')
-var xml2js = require('xml2js')
+var fs              = require('fs')
+var xpath           = require('xpath')
+var dom             = require('xmldom').DOMParser
+var elasticsearch   = require('elasticsearch')
+var op              = require('object-path')
 
-var elasticsearch = require('elasticsearch')
 var client = new elasticsearch.Client({
   host: 'localhost:9200',
   log: 'trace'
 })
 
 
-var parser = new xml2js.Parser()
+function recurseMappings(mappings, sourceNode, targetNode) {
+    mappings.forEach(function(mapping) {
+        var selected = xpath.select(mapping.sourceXpath, sourceNode)
+        if (!selected[0]) {
+            return
+        }
+        if (mapping.single === true) {
+            op.set(targetNode, mapping.targetPath, selected[0].childNodes.item(0).nodeValue)
+        }
+        else {
+            for (var j=0; j<selected.length; j++) {
+                if (mapping.mappings && mapping.mappings.length > 0) {
+                    op.set(targetNode, mapping.targetPath.concat(j), {})
+                    recurseMappings(mapping.mappings, selected[j], op.get(targetNode, mapping.targetPath.concat(j)))
+                }
+                else {
+                    op.set(targetNode, mapping.targetPath.concat(j), selected[j].childNodes.item(0).nodeValue)
+                }
+            }
+        }
+    })
+}
 
-fs.readFile('mili_mt.xml', 'utf16le', function(err, data) {
-    if (err) {
-        return console.log(err)
-    }
-    // console.log(data)
-    parser.parseString(data, function (err, result) {
+function index_db(db) {
+    fs.readFile(db.file, db.encoding, function(err, xml) {
         if (err) {
             return console.log(err)
         }
-        var conceptGroupA = result.mtf.conceptGrp
 
-        conceptGroupA = conceptGroupA.map(function(c) {
-            return {
-                index: {_index: 'concept', _type: 'concept', _id: c.concept[0] },
-                conceptGroup: c
-            }
+        var doc = new dom().parseFromString(xml, 'text/xml')
+        // var nodes = xpath.select(db.elementSelector, doc)
+        var sourceNodes = doc.getElementsByTagName(db.elementSelector)
+        var targetNodes = []
+
+        for (var i=0; i<sourceNodes.length; i++) {
+            var sourceNode = sourceNodes.item(i)
+            var targetNode = {}
+            targetNodes.push(targetNode)
+            recurseMappings(db.mappings, sourceNode, targetNode)
+        }
+        // console.log(JSON.stringify(targetNodes, null, 4))
+
+        var bulk = []
+        targetNodes.forEach(function(c) {
+            bulk.push({ index: {
+                _index: 'concepts',
+                _type: 'concept',
+                _id: db.name + '_' + c.id
+            } })
+            bulk.push(c)
         })
 
-        client.bulk({ body: conceptGroupA })
+        client.bulk({ body: bulk })
 
         console.log('Done')
     })
+}
+
+var databases = require('./databases.json')
+
+databases.forEach( function(db) {
+    // console.log('indexing', db)
+    index_db(db)
 })
